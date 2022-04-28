9dc68122f9ec481d98add0deb3b3f83bc379ccd8
Sergei Gribanov <ssgribanov@gmail.com>
2022-04-28 17:56:08 +0700

    add simple kinfit wrappers

---
+++ utils/run_kinfit.py
@@ -0,0 +1,59 @@
+import os
+import ipywidgets as widgets
+from IPython.display import display
+from pprint import pprint
+import ROOT
+
+
+def build_custom_hypo(hypo_cpp_path,
+                      rootlogon_path='/home/hep/packages/kfcmd/share/kfcmd/rootlogon.C'):
+    out = widgets.Output(layout={'border': '1px solid black'})
+    display(out)
+    with out:
+        print('Loading macro: {}'.format(rootlogon_path))
+
+    ROOT.gInterpreter.ProcessLine('gROOT->LoadMacro("{}")'.format(rootlogon_path))
+
+    with out:
+        print('Building: .L {}++'.format(hypo_cpp_path))
+
+    ROOT.gInterpreter.ProcessLine('.L {}++'.format(hypo_cpp_path))
+
+
+def run_kinfit(
+        input_path,
+        output_path='test.root',
+        mfield=1.3,
+        rootlogon_path='/home/hep/packages/kfcmd/share/kfcmd/rootlogon.C'):
+    varibles = locals()
+    out = widgets.Output(layout={'border': '1px solid black'})
+    display(out)
+    with out:
+        pprint(varibles)
+
+    with out:
+        print('Loading macro: {}'.format(rootlogon_path))
+
+    ROOT.gInterpreter.ProcessLine('gROOT->LoadMacro("{}")'.format(rootlogon_path))
+    with out:
+        print('Building: .L TrPh.C++')
+
+    ROOT.gInterpreter.ProcessLine(".L TrPh.C++")
+    with out:
+        print('Opening input file: {}'.format(input_path))
+
+    ROOT.gInterpreter.ProcessLine('TFile fl("{}", "read")'.format(input_path))
+    with out:
+        print('Creating TrPh object...')
+
+    ROOT.gInterpreter.ProcessLine('TrPh a(tr_ph)')
+
+    with out:
+        print('Starting kinfit loop...')
+
+    ROOT.gInterpreter.ProcessLine('a.Loop("{output_path}", {mfield})'.format(output_path=output_path, mfield=mfield))
+
+    with out:
+        print('Closing input file...')
+
+    ROOT.gInterpreter.ProcessLine('fl.Close()')
