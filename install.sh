a5bb0900bfcb64c52f2ab292c6129bb282a39f8d
Sergei Gribanov <ssgribanov@gmail.com>
2022-05-09 18:42:19 +0700

    a number of changes

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ mkdir $SOURCE_DIR
 mkdir $BUILD_DIR
 cd $SOURCE_DIR
 
-git clone --branch develop https://github.com/sergeigribanov/KFBase
-git clone --branch develop https://github.com/sergeigribanov/KFCmd
+git clone --branch develop1 https://github.com/sergeigribanov/KFBase
+git clone --branch develop1 https://github.com/sergeigribanov/KFCmd
 git clone https://github.com/sergeigribanov/gaussgen
-git clone --branch develop https://github.com/sergeigribanov/KFCmd5PiTest
 
 source $PKG_DIR/root/bin/thisroot.sh
 
@@ -30,9 +29,3 @@ cd $BUILD_DIR/gaussgen
 cmake  -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=$PKG_DIR/gaussgen $SOURCE_DIR/gaussgen
 make -j8
 make install
-
-# mkdir $BUILD_DIR/KFCmd5PiTest
-# cd $BUILD_DIR/KFCmd5PiTest
-# cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=$PKG_DIR/KFCmd5PiTest $SOURCE_DIR/KFCmd5PiTest
-# make -j8
-# make install
